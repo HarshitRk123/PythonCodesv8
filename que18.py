a48aef1a98c6a1efcd3acf9e2ac11ab8c4704283
Harshit <harsshitrk0120@gmail.com>
2024-10-18 22:47:19 +0530

    Add files via upload

---
+++ que18.py
@@ -0,0 +1,8 @@
+def calculate_seconds_in_a_day():
+    hours_per_day = 24
+    minutes_per_hour = 60
+    seconds_per_minute = 60
+    seconds_in_a_day = hours_per_day * minutes_per_hour * seconds_per_minute
+
+    print(f"Number of seconds in a day: {seconds_in_a_day}")
+calculate_seconds_in_a_day()
